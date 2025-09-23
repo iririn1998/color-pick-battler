5dd8698941589798866f7ce26d83c921f4d5da58
Shungo-I <iririn199810710@gmail.com>
2025-09-24 00:03:05 +0900

    fix: biome修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,9 @@
-import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from "@mantine/core";
+import {
+  ColorSchemeScript,
+  MantineProvider,
+  mantineHtmlProps,
+} from "@mantine/core";
+import "@mantine/core/styles.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
