30ab041cff90b8db83d675e241da0c8befc5acad
Shungo-I <iririn199810710@gmail.com>
2025-11-03 05:06:04 +0900

    feat: 新しいNavigationButtonコンポーネントを追加し、スタイルとストーリーを作成

---
+++ src/components/NavigationButton/index.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@mantine/core";
+import clsx from "clsx";
+import type { ComponentPropsWithoutRef, FC } from "react";
+import type { IconType } from "react-icons";
+import styles from "./index.module.css";
+
+export type NavigationButtonProps = Omit<
+  ComponentPropsWithoutRef<typeof Button>,
+  "children"
+> & {
+  icon: IconType;
+  className?: string;
+};
+
+export const NavigationButton: FC<NavigationButtonProps> = ({
+  icon: Icon,
+  className,
+  ...rest
+}) => {
+  return (
+    <Button
+      {...rest}
+      className={clsx(styles.navigationButton, className)}
+      variant="filled"
+    >
+      <Icon className={styles.icon} />
+    </Button>
+  );
+};
